5633d6984deb8ab9604f1d4e327519bfc9a7622c
Rishabh42 <rishabhthaney@gmail.com>
2018-07-16 16:01:13 +0530

    new repo

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+var Loyalty = artifacts.require("./loyalty.sol");
+
+module.exports = function(deployer) {
+  //deployer.deploy(ConvertLib);
+  //deployer.link(ConvertLib, MetaCoin);
+  //deployer.deploy(MetaCoin);
+  deployer.deploy(Loyalty);
+};
